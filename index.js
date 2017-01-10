7e9286587f9ddbde0345af3aed67fa86b800c1c2
KHA HOANG Y NGUYEN <ykha@cs.washington.edu>
2017-01-09 23:41:10 -0800

    update first entity

--- index.js
+++ index.js
@@ -96,6 +96,17 @@ const actions = {
     return context;
   }
 };
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
 
 
 const wit = new Wit({
