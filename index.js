c3f9821acac1c3655513966ccffbdf0a150daa39
Dolphin <ykha721@gmail.com>
2017-10-22 10:25:58 -0700

    hellow world

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ const firstEntityValue = (entities, entity) => {
                 } else {
                   //var returnOutput = "Hours of operation: " + body[0]["Hours_of_Operation"] + ". Exception location: " + body[0]["Exceptions_Location"] +". Peak Time: " + body[0]["Peak_Time"] +
                   //". Smart Meter: "  + body[0]["Smart_Meter"] + ". Rate: " + body[0]["Rate"];                  
-                    console.log(body);
+                    console.log("bodyyyy",body);
                     retVal = body;
                     //console.log("body0"+body.City);
                      //fbMessage(sender, body).catch(console.error);
