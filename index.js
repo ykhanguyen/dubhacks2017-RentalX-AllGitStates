2a24288b7169c5860331f6bd7b63e1767093a182
Dolphin <ykha721@gmail.com>
2017-10-22 14:27:41 -0700

    flda

--- index.js
+++ index.js
@@ -93,8 +93,10 @@ function firstEntityValue (entities, entity) {
                 let option2 = suggestion(age, gender, sport, travel);
 
                 function suggestion(age, gender, sport, travel) {
+                  url = "http://127.0.0.1:5000/" + age + "/0.8/1/" + gender + "/" + sport + "/" + travel;
+                  console.log("URL: " + url);
                   return {
-                    "url": "http://127.0.0.1:5000/" + age + "/0.8/1/" + gender + "/" + sport + "/" + travel,
+                    "url": url,
                     "method": "GET",
                   }
                 }
